9c11246ffe899c1c2d7fde53db65747ca908b34d
Valentina Svichnik <svichnikvalentina@gmail.com>
2023-05-18 22:12:12 +0300

    before authorization

--- mtv/backend_api/serializer.py
+++ mtv/backend_api/serializer.py
@@ -11,6 +11,11 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = '__all__'
 
+# class CommentSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Comment
+#         fields = '__all__'
+
 class CategoryDetailSerializer(serializers.ModelSerializer):
     products = serializers.SerializerMethodField()
 
@@ -21,6 +26,22 @@ class CategoryDetailSerializer(serializers.ModelSerializer):
     @staticmethod
     def get_products(obj):
         return  ProductSerializer(Product.objects.filter(category=obj), many=True).data
+
+class ProductDetailSerializer(serializers.ModelSerializer):
+    category = serializers.SerializerMethodField()
+#     comment = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    @staticmethod
+    def get_category(obj):
+        return  CategorySerializer(Category.objects.filter(product=obj), many=True).data
+
+#     @staticmethod
+#     def get_comment(obj):
+#         return  CommentSerializer(Comment.objects.filter(product=obj), many=True).data
 
 class PartnerSerializer(serializers.ModelSerializer):
     class Meta:
--- mtv/backend_api/views.py
+++ mtv/backend_api/views.py
@@ -12,44 +12,27 @@ from .serializer import (
     ProductSerializer,
     CategorySerializer,
     PartnerSerializer,
-    CategoryDetailSerializer
+    CategoryDetailSerializer,
+    ProductDetailSerializer,
+#     CommentSerializer,
 )
 from rest_framework.response import Response
 
-# class ProductView(APIView):
-#     def get(self, request):
-#         output = [
-#             {
-#                 "title": output.title,
-#                 "price": output.price
-#             } for output in Product.objects.all()
-#         ]
-#         return Response(output)
-#
-#     def post(self, request):
-#         serializer = ProductSerializer(data=request.data)
-#         if serializer.is_valid(raise_exception=True):
-#             serializer.save()
-#             return Response(serializer.data)
-#
-#
-# class CategoryView(APIView):
-#     def get(self, request):
-#         output = [
-#             {
-#                 "name": output.name,
-#                 "targeted": output.targeted,
-#                 "slug": output.slug,
-#             } for output in Category.objects.all()
-#         ]
-#         return Response(output)
 
-# def category_detail(request, id):
-#     return render(request, 'index.html', {})
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    action_to_serializer = {
+        "retrieve": ProductDetailSerializer
+    }
+
+    def get_serializer_class(self):
+        return self.action_to_serializer.get (
+            self.action,
+            self.serializer_class
+        )
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
